efb0d680d8cbfc176d65a25738e238718b7ad0eb
pi-null-mezon <pi-null-mezon@yandex.ru>
2018-10-12 11:12:27 +0300

    updt

--- Proteins/Trainer/customnetwork.h
+++ Proteins/Trainer/customnetwork.h
@@ -10,7 +10,7 @@ template <template <int,template<typename>class,int,typename> class block, int N
 using residual = add_prev1<block<N,BN,1,tag1<SUBNET>>>;
 
 template <template <int,template<typename>class,int,typename> class block, int N, template<typename>class BN, typename SUBNET>
-using residual_down = add_prev2<max_pool<2,2,2,2,skip1<tag2<block<N,BN,2,tag1<SUBNET>>>>>>;
+using residual_down = add_prev2<avg_pool<2,2,2,2,skip1<tag2<block<N,BN,2,tag1<SUBNET>>>>>>;
 
 template <int N, template <typename> class BN, int stride, typename SUBNET>
 using block  = BN<con<N,3,3,1,1,relu<BN<con<N,3,3,stride,stride,SUBNET>>>>>;
@@ -21,7 +21,7 @@ template <int N, typename SUBNET> using res_down  = relu<residual_down<block,N,b
 template <int N, typename SUBNET> using ares_down = relu<residual_down<block,N,affine,SUBNET>>;
 // ----------------------------------------------------------------------------------------
 #define FNUM 32
-#define IMG_SIZE 512
+#define IMG_SIZE 256
 template <typename SUBNET> using level2 = res_down<FNUM*32,SUBNET>;
 template <typename SUBNET> using level3 = res<FNUM*16,res_down<FNUM*16,SUBNET>>;
 template <typename SUBNET> using level4 = res<8*FNUM,res_down<8*FNUM,SUBNET>>;
@@ -34,25 +34,17 @@ template <typename SUBNET> using alevel4 = ares<8*FNUM,ares_down<8*FNUM,SUBNET>>
 template <typename SUBNET> using alevel5 = ares<4*FNUM,ares_down<4*FNUM,SUBNET>>;
 template <typename SUBNET> using alevel6 = ares<2*FNUM,ares<2*FNUM,ares_down<2*FNUM,SUBNET>>>;
 // training network type
-using net_type =    loss_multimulticlass_log<fc<56,avg_pool_everything<
-                            level2<
-                            level3<
-                            level4<
-                            level5<
+using net_type =    loss_multimulticlass_log<fc<56,max_pool_everything<
                             level6<
                             max_pool<3,3,2,2,relu<bn_con<con<FNUM,7,7,2,2,
                             input<matrix<float>>
-                            >>>>>>>>>>>>;
+                            >>>>>>>>;
 
 // testing network type (replaced batch normalization with fixed affine transforms)
-using anet_type =   loss_multimulticlass_log<fc<56,avg_pool_everything<
-                            alevel2<
-                            alevel3<
-                            alevel4<
-                            alevel5<
+using anet_type =   loss_multimulticlass_log<fc<56,max_pool_everything<
                             alevel6<
                             max_pool<3,3,2,2,relu<affine<con<FNUM,7,7,2,2,
                             input<matrix<float>>
-                            >>>>>>>>>>>>;
+                            >>>>>>>>;
 
 #endif // CUSTOMNETWORK_H
