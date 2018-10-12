7127fece034cdeba37593038df4d455b1a75aaf4
pi-null-mezon <pi-null-mezon@yandex.ru>
2018-10-12 11:22:27 +0300

    updt

--- Proteins/Trainer/customnetwork.h
+++ Proteins/Trainer/customnetwork.h
@@ -34,17 +34,21 @@ template <typename SUBNET> using alevel4 = ares<8*FNUM,ares_down<8*FNUM,SUBNET>>
 template <typename SUBNET> using alevel5 = ares<4*FNUM,ares_down<4*FNUM,SUBNET>>;
 template <typename SUBNET> using alevel6 = ares<2*FNUM,ares<2*FNUM,ares_down<2*FNUM,SUBNET>>>;
 // training network type
-using net_type =    loss_multimulticlass_log<fc<56,max_pool_everything<
+using net_type =    loss_multimulticlass_log<fc<56,avg_pool_everything<
+                            level4<
+                            level5<
                             level6<
-                            max_pool<3,3,2,2,relu<bn_con<con<FNUM,7,7,2,2,
+                            avg_pool<2,2,2,2,relu<bn_con<con<FNUM,5,5,1,1,
                             input<matrix<float>>
-                            >>>>>>>>;
+                            >>>>>>>>>>;
 
 // testing network type (replaced batch normalization with fixed affine transforms)
-using anet_type =   loss_multimulticlass_log<fc<56,max_pool_everything<
+using anet_type =   loss_multimulticlass_log<fc<56,avg_pool_everything<
+                            alevel4<
+                            alevel5<
                             alevel6<
-                            max_pool<3,3,2,2,relu<affine<con<FNUM,7,7,2,2,
+                            avg_pool<2,2,2,2,relu<affine<con<FNUM,5,5,1,1,
                             input<matrix<float>>
-                            >>>>>>>>;
+                            >>>>>>>>>>;
 
 #endif // CUSTOMNETWORK_H
