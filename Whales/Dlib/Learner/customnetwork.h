04af43236b302a433ada1c5ab3b0183efc53e0cb
pi-null-mezon <pi-null-mezon@yandex.ru>
2018-12-07 09:40:06 +0300

    new arch

--- Whales/Dlib/Learner/customnetwork.h
+++ Whales/Dlib/Learner/customnetwork.h
@@ -8,6 +8,49 @@
 
 namespace dlib {
 
+template <int N, template <typename> class BN, typename SUBNET>
+using block  = relu<BN<con<N,3,3,1,1,relu<BN<con<4*N,1,1,1,1,SUBNET>>>>>>;
+
+template <int N, int K, template <typename> class BN, typename SUBNET>
+using dense_block2 = relu<BN<con<N,1,1,1,1, concat3<tag3,tag2,tag1,  tag3<block<K,BN,concat2<tag2,tag1, tag2<block<K,BN, tag1<SUBNET>>>>>>>>>>;
+
+template <int N, int K, template <typename> class BN, typename SUBNET>
+using dense_block3 = relu<BN<con<N,1,1,1,1, concat4<tag4,tag3,tag2,tag1, tag4<block<K,BN,concat3<tag3,tag2,tag1,  tag3<block<K,BN,concat2<tag2,tag1, tag2<block<K,BN, tag1<SUBNET>>>>>>>>>>>>>;
+
+template <int N, int K, template <typename> class BN, typename SUBNET>
+using dense_block4 = relu<BN<con<N,1,1,1,1, concat5<tag5,tag4,tag3,tag2,tag1,  tag5<block<K,BN,concat4<tag4,tag3,tag2,tag1, tag4<block<K,BN,concat3<tag3,tag2,tag1,  tag3<block<K,BN,concat2<tag2,tag1, tag2<block<K,BN, tag1<SUBNET>>>>>>>>>>>>>>>>;
+
+template <int N, int K, typename SUBNET> using dense2  = dense_block2<N,K,bn_con,SUBNET>;
+template <int N, int K, typename SUBNET> using dense3  = dense_block3<N,K,bn_con,SUBNET>;
+template <int N, int K, typename SUBNET> using dense4  = dense_block4<N,K,bn_con,SUBNET>;
+
+template <int N, int K, typename SUBNET> using adense2 = dense_block2<N,K,affine,SUBNET>;
+template <int N, int K, typename SUBNET> using adense3 = dense_block3<N,K,affine,SUBNET>;
+template <int N, int K, typename SUBNET> using adense4 = dense_block4<N,K,affine,SUBNET>;
+// ----------------------------------------------------------------------------------------
+
+// training network type
+using net_type =    loss_metric<fc_no_bias<64,
+                            avg_pool_everything<dense2<128,32,dense3<128,32,dense4<128,32,
+                            max_pool<2,2,2,2,dense3<128,32,dense4<128,32,
+                            max_pool<2,2,2,2,dense3<128,32,dense4<128,32,
+                            max_pool<2,2,2,2,dense2<128,32,dense3<128,32,dense4<128,32,
+                            max_pool<2,2,2,2,dense3<128,32,dense4<128,32,
+                            max_pool<3,3,2,2,relu<bn_con<con<32,7,7,2,2,
+                            input<matrix<float>>
+                            >>>>>>>>>>>>>>>>>>>>>>>;
+
+// testing network type (replaced batch normalization with fixed affine transforms)
+using anet_type =   loss_metric<fc_no_bias<64,
+                            avg_pool_everything<adense2<128,32,adense3<128,32,adense4<128,32,
+                            max_pool<2,2,2,2,adense3<128,32,adense4<128,32,
+                            max_pool<2,2,2,2,adense3<128,32,adense4<128,32,
+                            max_pool<2,2,2,2,adense2<128,32,adense3<128,32,adense4<128,32,
+                            max_pool<2,2,2,2,adense3<128,32,adense4<128,32,
+                            max_pool<3,3,2,2,relu<bn_con<con<32,7,7,2,2,
+                            input<matrix<float>>
+                            >>>>>>>>>>>>>>>>>>>>>>>;
+/*
 template <template <int,template<typename>class,int,typename> class block, int N, template<typename>class BN, typename SUBNET>
 using residual = add_prev1<block<N,BN,1,tag1<SUBNET>>>;
 
@@ -58,6 +101,7 @@ using anet_type = loss_metric<fc_no_bias<128,avg_pool_everything<
                             relu<affine<con<16,7,7,2,2,
                             input<matrix<float>>
                             >>>>>>>>>>>;
+*/
 }
 
 #endif // CUSTOMNETWORK_H
