00275d94c2350fcc337ec8b556b4d44702ef73cf
pi-null-mezon <pi-null-mezon@yandex.ru>
2020-05-04 16:01:28 +0300

    pupil detector added

---
+++ Pupil/Learner/customnetwork.h
@@ -0,0 +1,50 @@
+#ifndef CUSTOMNETWORK_H
+#define CUSTOMNETWORK_H
+
+#include <dlib/dnn.h>
+
+#define IMG_WIDTH 48
+#define IMG_HEIGHT 48
+
+using namespace dlib;
+
+// ----------------------------------------------------------------------------------------
+
+template <template <int,template<typename>class,int,typename> class block, int N, template<typename>class BN, typename SUBNET>
+using residual = add_prev1<block<N,BN,1,tag1<SUBNET>>>;
+
+template <template <int,template<typename>class,int,typename> class block, int N, template<typename>class BN, typename SUBNET>
+using residual_down = add_prev2<avg_pool<2,2,2,2,skip1<tag2<block<N,BN,2,tag1<SUBNET>>>>>>;
+
+template <int N, template <typename> class BN, int stride, typename SUBNET>
+using block  = BN<con<N,3,3,1,1,relu<BN<con<N,3,3,stride,stride,SUBNET>>>>>;
+
+
+template <int N, typename SUBNET> using res       = relu<residual<block,N,bn_con,SUBNET>>;
+template <int N, typename SUBNET> using ares      = relu<residual<block,N,affine,SUBNET>>;
+template <int N, typename SUBNET> using res_down  = relu<residual_down<block,N,bn_con,SUBNET>>;
+template <int N, typename SUBNET> using ares_down = relu<residual_down<block,N,affine,SUBNET>>;
+
+// ----------------------------------------------------------------------------------------
+
+template <typename SUBNET> using level3 = res<32,res<32,res_down<32,SUBNET>>>;
+template <typename SUBNET> using level4 = res<16,res<16,SUBNET>>;
+
+template <typename SUBNET> using alevel3 = ares<32,ares<32,ares_down<32,SUBNET>>>;
+template <typename SUBNET> using alevel4 = ares<16,ares<16,SUBNET>>;
+
+// training network type
+using net_type = loss_mean_squared_multioutput<fc<3,avg_pool_everything<
+                                        level3<
+                                        level4<
+                                        max_pool<3,3,2,2,relu<bn_con<con<8,3,3,1,1,
+                                        input_rgb_image >>>>>>>>>;
+
+// testing network type (replaced batch normalization with fixed affine transforms)
+using anet_type = loss_mean_squared_multioutput<fc<3,avg_pool_everything<
+                                         alevel3<
+                                         alevel4<
+                                         max_pool<3,3,2,2,relu<affine<con<8,3,3,1,1,
+                                         input_rgb_image >>>>>>>>>;
+
+#endif // CUSTOMNETWORK_H
